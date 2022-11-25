d7d107c587b32c1384164292fec857cf06b78062
gissily <gissily@outlook.com>
2022-11-25 22:38:02 +0800

    fix

--- jdk/eclipse-temurin/17-jdk-jammy.sh
+++ jdk/eclipse-temurin/17-jdk-jammy.sh
@@ -19,11 +19,11 @@ docker image tag eclipse-temurin:17-jdk-jammy-${TAG_VERSION} ${CI_REGISTRY}/opca
 docker push ${CI_REGISTRY}/opcal/eclipse-temurin:17-jdk-jammy-${TIMESTAMP}
 docker push ${CI_REGISTRY}/opcal/eclipse-temurin:17-jdk-jammy
 
-## tag 11-jdk
+## tag 17-jdk
 docker image tag eclipse-temurin:17-jdk-jammy-${TAG_VERSION} ${CI_REGISTRY}/opcal/eclipse-temurin:17-jdk
 docker push ${CI_REGISTRY}/opcal/eclipse-temurin:17-jdk
 docker rmi -f ${CI_REGISTRY}/opcal/eclipse-temurin:17-jdk
-## tag 11-jdk
+## tag 17-jdk
 
 docker rmi -f ${CI_REGISTRY}/opcal/eclipse-temurin:17-jdk-jammy
 docker rmi -f ${CI_REGISTRY}/opcal/eclipse-temurin:17-jdk-jammy-${TIMESTAMP}
