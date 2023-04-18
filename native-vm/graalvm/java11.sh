f7f3cbf03475c69afc2bf9374dc608b318aee6de
gissily <gissily@outlook.com>
2023-04-18 16:43:47 +0800

    fix JAVA_VERSION vars

--- native-vm/graalvm/java11.sh
+++ native-vm/graalvm/java11.sh
@@ -5,14 +5,13 @@ set -e
 echo " "
 echo " "
 
-# GRL_VERSION=22.0.0.2.r11-grl
 BASE_IMAGE=ghcr.io/opcal/ubuntu:jammy
 JAVA_VERSION=java11
 TARGETPLATFORM=linux-amd64
 GRAALVM_VERSION=$(curl https://api.github.com/repos/graalvm/graalvm-ce-builds/releases/latest | grep tag_name | cut -d '"' -f 4 | cut -d '-' -f 2)
 FIRST_VERSION=$(echo ${GRAALVM_VERSION} | cut -d '.' -f 1)
 
-echo "build graalvm-java11-${FIRST_VERSION} start"
+echo "build graalvm-${JAVA_VERSION}-${FIRST_VERSION} start"
 
 # graalvm-java11
 docker build \
@@ -20,20 +19,20 @@ docker build \
     --build-arg JAVA_VERSION=${JAVA_VERSION} \
     --build-arg TARGETPLATFORM=${TARGETPLATFORM} \
     --build-arg GRAALVM_VERSION=${GRAALVM_VERSION} \
-    -t graalvm:java11-${TAG_VERSION} \
+    -t graalvm:${JAVA_VERSION}-${TAG_VERSION} \
     -f ${PROJECT_DIR}/native-vm/graalvm/base/Dockerfile . --no-cache
-docker image tag graalvm:java11-${TAG_VERSION} ${CI_REGISTRY}/opcal/graalvm:java11-${GRAALVM_VERSION}-${TIMESTAMP}
-docker image tag graalvm:java11-${TAG_VERSION} ${CI_REGISTRY}/opcal/graalvm:java11-${FIRST_VERSION}
-docker image tag graalvm:java11-${TAG_VERSION} ${CI_REGISTRY}/opcal/graalvm:java11-${GRAALVM_VERSION}
-docker push ${CI_REGISTRY}/opcal/graalvm:java11-${GRAALVM_VERSION}-${TIMESTAMP}
-docker push ${CI_REGISTRY}/opcal/graalvm:java11-${FIRST_VERSION}
-docker push ${CI_REGISTRY}/opcal/graalvm:java11-${GRAALVM_VERSION}
+docker image tag graalvm:${JAVA_VERSION}-${TAG_VERSION} ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${GRAALVM_VERSION}-${TIMESTAMP}
+docker image tag graalvm:${JAVA_VERSION}-${TAG_VERSION} ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${FIRST_VERSION}
+docker image tag graalvm:${JAVA_VERSION}-${TAG_VERSION} ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${GRAALVM_VERSION}
+docker push ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${GRAALVM_VERSION}-${TIMESTAMP}
+docker push ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${FIRST_VERSION}
+docker push ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${GRAALVM_VERSION}
 
-docker rmi -f ${CI_REGISTRY}/opcal/graalvm:java11-${GRAALVM_VERSION}
-docker rmi -f ${CI_REGISTRY}/opcal/graalvm:java11-${FIRST_VERSION}
-docker rmi -f ${CI_REGISTRY}/opcal/graalvm:java11-${GRAALVM_VERSION}-${TIMESTAMP}
-docker rmi -f graalvm:java11-${TAG_VERSION}
+docker rmi -f ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${GRAALVM_VERSION}
+docker rmi -f ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${FIRST_VERSION}
+docker rmi -f ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${GRAALVM_VERSION}-${TIMESTAMP}
+docker rmi -f graalvm:${JAVA_VERSION}-${TAG_VERSION}
 
-echo "build graalvm-java11-${FIRST_VERSION} finished"
+echo "build graalvm-${JAVA_VERSION}-${FIRST_VERSION} finished"
 echo " "
 echo " "
--- native-vm/graalvm/java19.sh
+++ native-vm/graalvm/java19.sh
@@ -12,7 +12,7 @@ TARGETPLATFORM=linux-amd64
 GRAALVM_VERSION=$(curl https://api.github.com/repos/graalvm/graalvm-ce-builds/releases/latest | grep tag_name | cut -d '"' -f 4 | cut -d '-' -f 2)
 FIRST_VERSION=$(echo ${GRAALVM_VERSION} | cut -d '.' -f 1)
 
-echo "build graalvm-java19-${FIRST_VERSION} start"
+echo "build graalvm-${JAVA_VERSION}-${FIRST_VERSION} start"
 
 # graalvm-java19
 docker build \
@@ -20,20 +20,20 @@ docker build \
     --build-arg JAVA_VERSION=${JAVA_VERSION} \
     --build-arg TARGETPLATFORM=${TARGETPLATFORM} \
     --build-arg GRAALVM_VERSION=${GRAALVM_VERSION} \
-    -t graalvm:java19-${TAG_VERSION} \
+    -t graalvm:${JAVA_VERSION}-${TAG_VERSION} \
     -f ${PROJECT_DIR}/native-vm/graalvm/base/Dockerfile . --no-cache
-docker image tag graalvm:java19-${TAG_VERSION} ${CI_REGISTRY}/opcal/graalvm:java19-${GRAALVM_VERSION}-${TIMESTAMP}
-docker image tag graalvm:java19-${TAG_VERSION} ${CI_REGISTRY}/opcal/graalvm:java19-${FIRST_VERSION}
-docker image tag graalvm:java19-${TAG_VERSION} ${CI_REGISTRY}/opcal/graalvm:java19-${GRAALVM_VERSION}
-docker push ${CI_REGISTRY}/opcal/graalvm:java19-${GRAALVM_VERSION}-${TIMESTAMP}
-docker push ${CI_REGISTRY}/opcal/graalvm:java19-${FIRST_VERSION}
-docker push ${CI_REGISTRY}/opcal/graalvm:java19-${GRAALVM_VERSION}
+docker image tag graalvm:${JAVA_VERSION}-${TAG_VERSION} ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${GRAALVM_VERSION}-${TIMESTAMP}
+docker image tag graalvm:${JAVA_VERSION}-${TAG_VERSION} ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${FIRST_VERSION}
+docker image tag graalvm:${JAVA_VERSION}-${TAG_VERSION} ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${GRAALVM_VERSION}
+docker push ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${GRAALVM_VERSION}-${TIMESTAMP}
+docker push ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${FIRST_VERSION}
+docker push ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${GRAALVM_VERSION}
 
-docker rmi -f ${CI_REGISTRY}/opcal/graalvm:java19-${GRAALVM_VERSION}
-docker rmi -f ${CI_REGISTRY}/opcal/graalvm:java19-${FIRST_VERSION}
-docker rmi -f ${CI_REGISTRY}/opcal/graalvm:java19-${GRAALVM_VERSION}-${TIMESTAMP}
-docker rmi -f graalvm:java19-${TAG_VERSION}
+docker rmi -f ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${GRAALVM_VERSION}
+docker rmi -f ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${FIRST_VERSION}
+docker rmi -f ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${GRAALVM_VERSION}-${TIMESTAMP}
+docker rmi -f graalvm:${JAVA_VERSION}-${TAG_VERSION}
 
-echo "build graalvm-java19-${FIRST_VERSION} finished"
+echo "build graalvm-${JAVA_VERSION}-${FIRST_VERSION} finished"
 echo " "
 echo " "
