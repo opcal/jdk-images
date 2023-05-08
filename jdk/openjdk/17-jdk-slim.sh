f40879101d0869f5eb9e4d6823443282d7f5ef09
gissily <gissily@outlook.com>
2023-05-09 00:13:21 +0800

    build with platform

--- jdk/openjdk/17-jdk-slim.sh
+++ jdk/openjdk/17-jdk-slim.sh
@@ -9,18 +9,13 @@ echo 'build openjdk-17-jdk-slim start'
 BASE_IMAGE=openjdk:17-jdk-slim
 
 # 17-jdk-slim
-docker build \
+docker buildx build \
+    --platform ${PLATFORM} \
     --build-arg BASE_IMAGE=${BASE_IMAGE} \
-    -t openjdk:17-jdk-slim-${TAG_VERSION} \
+    --push \
+    -t ${CI_REGISTRY}/opcal/openjdk:17-jdk-slim-${TIMESTAMP} \
+    -t ${CI_REGISTRY}/opcal/openjdk:17-jdk-slim \
     -f ${PROJECT_DIR}/jdk/openjdk/base/Dockerfile . --no-cache
-docker image tag openjdk:17-jdk-slim-${TAG_VERSION} ${CI_REGISTRY}/opcal/openjdk:17-jdk-slim-${TIMESTAMP}
-docker image tag openjdk:17-jdk-slim-${TAG_VERSION} ${CI_REGISTRY}/opcal/openjdk:17-jdk-slim
-docker push ${CI_REGISTRY}/opcal/openjdk:17-jdk-slim-${TIMESTAMP}
-docker push ${CI_REGISTRY}/opcal/openjdk:17-jdk-slim
-
-docker rmi -f ${CI_REGISTRY}/opcal/openjdk:17-jdk-slim
-docker rmi -f ${CI_REGISTRY}/opcal/openjdk:17-jdk-slim-${TIMESTAMP}
-docker rmi -f openjdk:17-jdk-slim-${TAG_VERSION}
 
 echo 'build openjdk-17-jdk-slim finished'
 echo " "
