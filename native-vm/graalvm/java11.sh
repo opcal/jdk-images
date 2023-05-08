d4404e64ddacd597b007910acd61dec434cd61db
gissily <gissily@outlook.com>
2023-05-08 23:50:31 +0800

    build with platform

--- native-vm/graalvm/java11.sh
+++ native-vm/graalvm/java11.sh
@@ -7,31 +7,22 @@ echo " "
 
 BASE_IMAGE=ghcr.io/opcal/ubuntu:jammy
 JAVA_VERSION=java11
-TARGETPLATFORM=linux-amd64
 GRAALVM_VERSION=$(curl https://api.github.com/repos/graalvm/graalvm-ce-builds/releases/latest | grep tag_name | cut -d '"' -f 4 | cut -d '-' -f 2)
 FIRST_VERSION=$(echo ${GRAALVM_VERSION} | cut -d '.' -f 1)
 
 echo "build graalvm-${JAVA_VERSION}-${FIRST_VERSION} start"
 
 # graalvm-java11
-docker build \
+docker buildx build \
+    --platform ${PLATFORM} \
     --build-arg BASE_IMAGE=${BASE_IMAGE} \
     --build-arg JAVA_VERSION=${JAVA_VERSION} \
-    --build-arg TARGETPLATFORM=${TARGETPLATFORM} \
     --build-arg GRAALVM_VERSION=${GRAALVM_VERSION} \
-    -t graalvm:${JAVA_VERSION}-${TAG_VERSION} \
+    --push \
+    -t ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${GRAALVM_VERSION}-${TIMESTAMP} \
+    -t ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${FIRST_VERSION} \
+    -t ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${GRAALVM_VERSION} \
     -f ${PROJECT_DIR}/native-vm/graalvm/base/Dockerfile . --no-cache
-docker image tag graalvm:${JAVA_VERSION}-${TAG_VERSION} ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${GRAALVM_VERSION}-${TIMESTAMP}
-docker image tag graalvm:${JAVA_VERSION}-${TAG_VERSION} ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${FIRST_VERSION}
-docker image tag graalvm:${JAVA_VERSION}-${TAG_VERSION} ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${GRAALVM_VERSION}
-docker push ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${GRAALVM_VERSION}-${TIMESTAMP}
-docker push ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${FIRST_VERSION}
-docker push ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${GRAALVM_VERSION}
-
-docker rmi -f ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${GRAALVM_VERSION}
-docker rmi -f ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${FIRST_VERSION}
-docker rmi -f ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${GRAALVM_VERSION}-${TIMESTAMP}
-docker rmi -f graalvm:${JAVA_VERSION}-${TAG_VERSION}
 
 echo "build graalvm-${JAVA_VERSION}-${FIRST_VERSION} finished"
 echo " "
