cee02a2783c9401b96b1ba6f8554b93fe170ddcc
gissily <gissily@outlook.com>
2023-11-11 01:26:06 +0800

    fix latest

--- jdk/dind-jdk/dind-21.sh
+++ jdk/dind-jdk/dind-21.sh
@@ -10,6 +10,8 @@ echo " "
 echo " "
 echo 'build dind-jdk-21 start'
 
+LATEST=$(curl 'https://api.adoptium.net/v3/info/release_versions?architecture=x64&heap_size=normal&image_type=jdk&jvm_impl=hotspot&lts=true&os=alpine-linux&page=0&page_size=1&project=jdk&release_type=ga&semver=false&sort_method=DEFAULT&sort_order=DESC&vendor=eclipse&version=%5B21%2C22%29' | jq '.versions[0].openjdk_version' | cut -d '"' -f 2)
+
 FEATURE_VERSION=11
 
 API_URL=https://api.adoptium.net/v3/binary/latest/${FEATURE_VERSION}/ga/alpine-linux/x64/jdk/hotspot/normal/eclipse
@@ -18,7 +20,7 @@ API_URL=https://api.adoptium.net/v3/binary/latest/${FEATURE_VERSION}/ga/alpine-l
 # 21-dind-jdk
 docker build \
     --build-arg API_URL=${API_URL} \
-    --build-arg FEATURE_VERSION=${FEATURE_VERSION} \
+    --build-arg LATEST=${LATEST} \
     -t ${CI_REGISTRY}/opcal/dind-jdk:21 \
     -f ${PROJECT_DIR}/jdk/docker-jdk/base/Dockerfile . --no-cache
 
--- jdk/docker-jdk/17.sh
+++ jdk/docker-jdk/17.sh
@@ -6,6 +6,8 @@ echo " "
 echo " "
 echo 'build docker-jdk-17 start'
 
+LATEST=$(curl 'https://api.adoptium.net/v3/info/release_versions?architecture=x64&heap_size=normal&image_type=jdk&jvm_impl=hotspot&lts=true&os=alpine-linux&page=0&page_size=1&project=jdk&release_type=ga&semver=false&sort_method=DEFAULT&sort_order=DESC&vendor=eclipse&version=%5B17%2C18%29' | jq '.versions[0].openjdk_version' | cut -d '"' -f 2)
+
 FEATURE_VERSION=17
 
 API_URL=https://api.adoptium.net/v3/binary/latest/${FEATURE_VERSION}/ga/alpine-linux/x64/jdk/hotspot/normal/eclipse
@@ -13,7 +15,7 @@ API_URL=https://api.adoptium.net/v3/binary/latest/${FEATURE_VERSION}/ga/alpine-l
 # 17-docker-jdk
 docker build \
     --build-arg API_URL=${API_URL} \
-    --build-arg FEATURE_VERSION=${FEATURE_VERSION} \
+    --build-arg LATEST=${LATEST} \
     -t ${CI_REGISTRY}/opcal/docker-jdk:17 \
     -f ${PROJECT_DIR}/jdk/docker-jdk/base/Dockerfile . --no-cache
     
