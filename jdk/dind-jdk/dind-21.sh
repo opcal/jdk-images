429a91e113337841dfc79be1829514ef0e7307e8
gissily <gissily@outlook.com>
2024-12-01 17:07:59 +0800

    fix 21

--- jdk/dind-jdk/dind-21.sh
+++ jdk/dind-jdk/dind-21.sh
@@ -12,7 +12,7 @@ echo 'build dind-jdk-21 start'
 
 LATEST=$(curl 'https://api.adoptium.net/v3/info/release_versions?architecture=x64&heap_size=normal&image_type=jdk&jvm_impl=hotspot&lts=true&os=alpine-linux&page=0&page_size=1&project=jdk&release_type=ga&semver=false&sort_method=DEFAULT&sort_order=DESC&vendor=eclipse&version=%5B21%2C22%29' | jq '.versions[0].openjdk_version' | cut -d '"' -f 2)
 
-FEATURE_VERSION=11
+FEATURE_VERSION=21
 
 API_URL=https://api.adoptium.net/v3/binary/latest/${FEATURE_VERSION}/ga/alpine-linux/x64/jdk/hotspot/normal/eclipse
 
