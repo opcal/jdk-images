702f50246a860f9b547ba03bdfa143e8d8d398e5
gissily <gissily@outlook.com>
2025-03-31 13:39:13 +0800

    fix jdk download

--- native-vm/graalvm/21.sh
+++ native-vm/graalvm/21.sh
@@ -6,23 +6,19 @@ echo " "
 echo " "
 
 BASE_IMAGE=ghcr.io/opcal/ubuntu:jammy
-JAVA_VERSION=21.0.1
-JDK_VERSION=$(echo ${JAVA_VERSION} | cut -d '.' -f 1)
+JDK_VERSION=21
 
-echo "build graalvm-${JAVA_VERSION} start"
+echo "build graalvm-${JDK_VERSION} start"
 
 # graalvm-java21
 docker buildx build \
     --platform ${PLATFORM} \
     --build-arg BASE_IMAGE=${BASE_IMAGE} \
-    --build-arg JAVA_VERSION=${JAVA_VERSION} \
     --build-arg JDK_VERSION=${JDK_VERSION} \
     --push \
-    -t ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${TIMESTAMP} \
-    -t ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION} \
     -t ${CI_REGISTRY}/opcal/graalvm:${JDK_VERSION} \
     -f ${PROJECT_DIR}/native-vm/graalvm/base/Dockerfile . --no-cache
 
-echo "build graalvm-${JAVA_VERSION} finished"
+echo "build graalvm-${JDK_VERSION} finished"
 echo " "
 echo " "
