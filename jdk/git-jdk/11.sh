c3fe06b740811472867694d72a59555d595fcffe
gissily <gissily@outlook.com>
2025-03-31 13:59:53 +0800

    fix script

--- jdk/git-jdk/11.sh
+++ jdk/git-jdk/11.sh
@@ -17,11 +17,6 @@ docker buildx build \
     -t ${CI_REGISTRY}/opcal/git-jdk:11 \
     -f ${PROJECT_DIR}/jdk/git-jdk/base/Dockerfile . --no-cache
 
-docker push --all-tags ${CI_REGISTRY}/opcal/git-jdk
-
-docker rmi -f ${CI_REGISTRY}/opcal/git-jdk:11-${TIMESTAMP}
-docker rmi -f ${CI_REGISTRY}/opcal/git-jdk:11
-
 echo 'build git-jdk-11 finished'
 echo " "
 echo " "
