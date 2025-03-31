f5266443c6e6dd2e3ee7b760fb8a40727bd88273
gissily <gissily@outlook.com>
2025-03-31 13:20:40 +0800

    fix build platform

--- jdk/git-jdk/11.sh
+++ jdk/git-jdk/11.sh
@@ -9,8 +9,10 @@ echo 'build git-jdk-11 start'
 IMAGE=ghcr.io/opcal/eclipse-temurin:11-jdk
 
 # jdk-11
-docker build \
+docker buildx build \
+    --platform ${PLATFORM} \
     --build-arg BASE_IMAGE=${IMAGE} \
+    --push \
     -t ${CI_REGISTRY}/opcal/git-jdk:11-${TIMESTAMP} \
     -t ${CI_REGISTRY}/opcal/git-jdk:11 \
     -f ${PROJECT_DIR}/jdk/git-jdk/base/Dockerfile . --no-cache
--- jdk/git-jdk/17.sh
+++ jdk/git-jdk/17.sh
@@ -9,16 +9,13 @@ echo 'build git-jdk-17 start'
 IMAGE=ghcr.io/opcal/eclipse-temurin:17-jdk
 
 # jdk-17
-docker build \
+docker buildx build \
+    --platform ${PLATFORM} \
     --build-arg BASE_IMAGE=${IMAGE} \
+    --push \
     -t ${CI_REGISTRY}/opcal/git-jdk:17-${TIMESTAMP} \
     -t ${CI_REGISTRY}/opcal/git-jdk:17 \
     -f ${PROJECT_DIR}/jdk/git-jdk/base/Dockerfile . --no-cache
-
-docker push --all-tags ${CI_REGISTRY}/opcal/git-jdk
-
-docker rmi -f ${CI_REGISTRY}/opcal/git-jdk:17-${TIMESTAMP}
-docker rmi -f ${CI_REGISTRY}/opcal/git-jdk:17
 
 echo 'build git-jdk-17 finished'
 echo " "
