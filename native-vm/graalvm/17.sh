e677dd7001cd4cf2cfaefb77790026111b154b99
gissily <gissily@outlook.com>
2025-03-31 14:02:59 +0800

    fix jdk 17 download

--- native-vm/graalvm/17.sh
+++ native-vm/graalvm/17.sh
@@ -6,19 +6,23 @@ echo " "
 echo " "
 
 BASE_IMAGE=ghcr.io/opcal/ubuntu:jammy
+JAVA_VERSION=17.0.12
 JDK_VERSION=17
 
-echo "build graalvm-${JDK_VERSION} start"
+echo "build graalvm-${JAVA_VERSION} start"
 
 # graalvm-java17
 docker buildx build \
     --platform ${PLATFORM} \
     --build-arg BASE_IMAGE=${BASE_IMAGE} \
+    --build-arg JAVA_VERSION=${JAVA_VERSION} \
     --build-arg JDK_VERSION=${JDK_VERSION} \
     --push \
+    -t ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION}-${TIMESTAMP} \
+    -t ${CI_REGISTRY}/opcal/graalvm:${JAVA_VERSION} \
     -t ${CI_REGISTRY}/opcal/graalvm:${JDK_VERSION} \
-    -f ${PROJECT_DIR}/native-vm/graalvm/base/Dockerfile . --no-cache
+    -f ${PROJECT_DIR}/native-vm/graalvm/base/Dockerfile-17 . --no-cache
 
-echo "build graalvm-${JDK_VERSION} finished"
+echo "build graalvm-${JAVA_VERSION} finished"
 echo " "
 echo " "
