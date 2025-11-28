663f8f0a34bd393f99bdd55ea93e5e505f51d11b
gissily <gissily@outlook.com>
2025-11-28 11:52:56 +0800

    fix bake build

--- jdk/git-jdk/git-jdk.sh
+++ jdk/git-jdk/git-jdk.sh
@@ -10,19 +10,7 @@ docker buildx bake \
     -f ${PROJECT_DIR}/jdk/git-jdk/git-jdk-bake.hcl \
     --pull \
     --push \
-    --no-cache git-jdk-11
-
-docker buildx bake \
-    -f ${PROJECT_DIR}/jdk/git-jdk/git-jdk-bake.hcl \
-    --pull \
-    --push \
-    --no-cache git-jdk-17
-
-docker buildx bake \
-    -f ${PROJECT_DIR}/jdk/git-jdk/git-jdk-bake.hcl \
-    --pull \
-    --push \
-    --no-cache git-jdk-21
+    --no-cache
 
 echo 'build git-jdk finished'
 echo " "
